c2033cec6f3b0f04762a3a704c98754922fe46cf
const <plahetko.constantin@gmail.com>
2021-01-05 13:00:39 +0100

    decorator implementation

---
+++ src/Soy.java
@@ -0,0 +1,16 @@
+public class Soy extends CondimentDecorator{
+    Beverage beverage;
+
+    public Soy(Beverage beverage){
+        this.beverage = beverage;
+    }
+
+    public String getDescription(){
+        return beverage.getDescription() + "Soy";
+    }
+
+    @Override
+    public double cost() {
+        return 0.3 + beverage.cost();
+    }
+}
